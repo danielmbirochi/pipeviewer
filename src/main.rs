f72873da87d131d8e9ca0d2b52091342068016e3
Daniel <dani.mbirochi@gmail.com>
2023-07-02 17:19:30 -0300

    Implement error handling for writting to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, Read, Write, Result, ErrorKind};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main() {
+fn main() -> Result<()> {
     let silent = env::var("SILENT").unwrap_or_default().to_lowercase() == "true";
     let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
 
     loop {
-        let mut buffer = [0; CHUNK_SIZE];
         let bytes_read = match io::stdin().read(&mut buffer) {
             Ok(0) => break,
             Ok(n) => n,
             Err(err) => panic!("error reading stdin: {}", err),
         };
         total_bytes += bytes_read;
-        io::stdout().write_all(&buffer[..bytes_read]).unwrap();
+        if !silent {
+            eprint!("{}", total_bytes);
+        }
+
+        if let Err(e) = io::stdout().write_all(&buffer[..bytes_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+        }
     }
     if !silent {
         eprintln!("{} bytes read", total_bytes);
     }
+    Ok(())
 }
